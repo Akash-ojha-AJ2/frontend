55d8aca75d15608efcaa844c30f320eb80bd217d
Akash-ojha-AJ2 <jhak2594@gmail.com>
2024-12-21 12:08:39 +0530

    Update Login.jsx

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from './AuthContext';
-import './Login.css';
+import './login.css';
 
 const LoginPage = () => {
     const { login } = useAuth();
